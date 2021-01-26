2eea95925a922a2e2d2f95f04788cf0bda112595
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2021-01-26 14:40:28 -0800

    Update space_unit_text.sh

--- select_Apo_Holo/space_unit_text.sh
+++ select_Apo_Holo/space_unit_text.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#source /wynton/home/fraserlab/swankowicz/phenix-installer-dev-3594-intel-linux-2.6-x86_64-centos6/phenix-dev-3594/phenix_env.sh
-#export PATH="/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
-#source activate qfit3
-#which python
+source #Phenix Env
 
-line=$(echo $1 | tr '[:upper:]' '[:lower:]')
-mid=$(echo ${line:1:2})
-SPACE1=$(grep "^Space group number from file:" /wynton/group/fraser/swankowicz/mtz/191114/${line}.dump | awk '{print $6,$7}')
-UNIT1=$(grep "Unit cell:" /wynton/group/fraser/swankowicz/mtz/191114/${line}.dump | tail -n 1 | sed "s/[(),]//g" | awk '{print $3,$4,$5,$6,$7,$8}')
-RESO1=$(grep "^Resolution" /wynton/group/fraser/swankowicz/mtz/191114/${line}.dump | head -n 1 | awk '{print $4}')
-
-echo $line $RESO1 $SPACE1 $UNIT1 >> /wynton/group/fraser/swankowicz/space_unit_reso_191118.txt
+file=test.txt
+while read -r line; do
+      pdb=$(echo ${line} | tr '[:upper:]' '[:lower:]')     
+      phenix.cif_as_mtz /wynton/group/fraser/swankowicz/mtz/191114/r${pdb}sf.ent.gz --ignore_bad_sigmas --extend_flags --merge
+      phenix.mtz.dump /wynton/group/fraser/swankowicz/mtz/191114/r${pdb}sf.mtz > ${pdb}.dump
+      
+      SPACE1=$(grep "^Space group number from file:" /wynton/group/fraser/swankowicz/mtz/191114/${line}.dump | awk '{print $6,$7}')
+      UNIT1=$(grep "Unit cell:" /wynton/group/fraser/swankowicz/mtz/191114/${line}.dump | tail -n 1 | sed "s/[(),]//g" | awk '{print $3,$4,$5,$6,$7,$8}')
+      RESO1=$(grep "^Resolution" /wynton/group/fraser/swankowicz/mtz/191114/${line}.dump | head -n 1 | awk '{print $4}')
+      echo $line $RESO1 $SPACE1 $UNIT1 >> /wynton/group/fraser/swankowicz/space_unit_reso.txt
+done < $file
