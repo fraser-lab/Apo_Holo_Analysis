7ffec43993e4d2462192746ec373e72c2af70143
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2021-01-27 09:51:43 -0800

    Update refine_aniso_comp_omit.sh

--- refinement/refine_aniso_comp_omit.sh
+++ refinement/refine_aniso_comp_omit.sh
@@ -1,73 +1,53 @@
-#!/bin/bash
-#$ -l h_vmem=2G
-#$ -l mem_free=2G
-#$ -t 1-1 #307
-#$ -l h_rt=10:00:00
-#$ -pe smp 1
-
-
-#this script will run qfit based on the input PDB names you have.
 
 #________________________________________________INPUTS________________________________________________#
-#PDB_file=/wynton/group/fraser/swankowicz/COVID19/structure/COVID-19/PDB2.txt
-#working_dir='/wynton/group/fraser/swankowicz/COVID19/structure/COVID-19/' #where the folders are located
-echo $working_dir
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 
 #________________________________________________SET PATHS________________________________________________#
-source /wynton/group/fraser/swankowicz/phenix-installer-1.18rc7-3834-intel-linux-2.6-x86_64-centos6/phenix-1.18rc7-3834/phenix_env.sh
+source phenix env
 export PATH="/wynton/home/fraserlab/swankowicz/anaconda3/bin:$PATH"
-source activate qfit3
+source activate qfit #qFit 
 
 #__________________________________DETERMINE RESOLUTION AND (AN)ISOTROPIC REFINEMENT__________________________________
 PDB=$1
 echo $PDB
 
-mv "${PDB}-sf.mtz" ${PDB}.mtz
-echo "here"
-mtzmetadata=`phenix.mtz.dump ${PDB}.mtz`
-resrange=`grep "Resolution range:" <<< "${mtzmetadata}"`
 
-echo "${mtzmetadata}"
-echo "${resrange}"
-
-res=`echo "${resrange}" | cut -d " " -f 4 | cut -c 1-5`
-res1000=`echo $res | awk '{tot = $1*1000}{print tot }'`
-
-if (( $res1000 < 1550 )); then
-  adp='adp.individual.anisotropic="not (water or element H)"'
-else
-  adp='adp.individual.isotropic=all'
+if [[ -e "${PDB}-sf.cif" ]]; then #if SF is in cif format, convert
+  phenix.mtz_to_cif ${PDB}-sf.cif
+  mv "${PDB}-sf.mtz" ${PDB}.mtz  #make sure all mtz files are named the same way.
 fi
 
-#________________________________________________RUN QFIT________________________________________________#
-PDB=$1
-echo $PDB
+mtzmetadata=`phenix.mtz.dump ${PDB}.mtz`
+resrange=`grep "Resolution range:" <<< "${mtzmetadata}"` #determine the resolution of the structure
+echo ${resrange}
 
-#RUN REFINEMENT
+
+#_______________________________________________REFINEMENT PREP________________________________________________#
+
+
+
+#________________________________________________RUN REFINEMENT________________________________________________#
 if [[ -e "${PDB}_updated.pdb.ligands.cif" ]]; then
        echo '________________________________________________________Running refinement with ligand.________________________________________________________'
-         phenix.refine ${PDB}_updated.pdb.updated.pdb ${PDB}.mtz ${PDB}_updated.pdb.ligands.cif /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/finalize_aniso.params refinement.input.xray_data.r_free_flags.generate=True "$adp" output.prefix="${PDB}_aniso" #refinement.input.xray_data.labels=$xray_data_labels output.prefix="${PDB}_aniso"
+         phenix.refine ${PDB}_updated.pdb.updated.pdb ${PDB}.mtz ${PDB}_updated.pdb.ligands.cif finalize.params refinement.input.xray_data.r_free_flags.generate=True output.prefix="${PDB}" refinement.input.xray_data.labels=$xray_data_labels
 else
       echo '________________________________________________________Running refinement without ligand.________________________________________________________'
-        phenix.refine ${PDB}_updated.pdb.updated.pdb ${PDB}.mtz /wynton/home/fraserlab/swankowicz/190419_Phenix_ensemble/finalize_aniso.params refinement.input.xray_data.r_free_flags.generate=True "$adp" output.prefix="${PDB}_aniso" #refinement.input.xray_data.labels=$xray_data_labels output.prefix="${PDB}_aniso"
+        phenix.refine ${PDB}_updated.pdb.updated.pdb ${PDB}.mtz finalize.params refinement.input.xray_data.r_free_flags.generate=True output.prefix="${PDB}" refinement.input.xray_data.labels=$xray_data_labels
 fi
 
-#RUN COMPOSITE OMIT MAP
-    echo 'Starting Composite Omit Map'
-    if [[ -e composite_omit_map.mtz ]]; then
-        echo 'composite omit map already created'
+#__________________________________________RUN COMPOSITE OMIT MAP
+if [[ -e composite_omit_map.mtz ]]; then
+        echo 'Composite omit map already created'
+else
+    if [ ! -f ${PDB}.mtz ]; then
+         echo 'No mtz file'
     else
-        if [ ! -f ${PDB}.mtz ]; then
-                echo 'No mtz file'
-        else
-                phenix.mtz.dump ${PDB}-sf.mtz > ${PDB}_mtzdump.out
-                if grep -q FREE ${PDB}_mtzdump.out; then
-                        phenix.composite_omit_map ${PDB}.mtz ${PDB}_updated.pdb.updated_refine_001.pdb omit-type=refine nproc=1 
-                else
-                        phenix.composite_omit_map ${PDB}.mtz ${PDB}_updated.pdb.updated_refine_001.pdb omit-type=refine nproc=1 r_free_flags.generate=True
-                fi
-        fi
-    fi
+         if grep -q FREE ${PDB}_mtzdump.out; then #if there are r_free_flags, don't re-generate.
+             phenix.composite_omit_map ${PDB}.mtz ${PDB}_updated.pdb.updated_refine_001.pdb omit-type=refine
+         else
+             phenix.composite_omit_map ${PDB}.mtz ${PDB}_updated.pdb.updated_refine_001.pdb omit-type=refine r_free_flags.generate=True
+         fi
+   fi
+fi
