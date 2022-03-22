aa9cb3434a606bd09f06719153099e1a56a8c1db
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-03-22 15:27:53 -0700

    Update refinement_stat.sh

--- QC/refinement_stat.sh
+++ QC/refinement_stat.sh
@@ -5,5 +5,7 @@ PDB_file=PDB.txt #this is a list of each PDB that completed re-refinement, qFit,
 
 while read PDB; do 
   echo ${PDB}
+  refinement.py ${base_dir}/${holo}/${holo} ${base_dir}/${apo}/${apo} ${holo} ${apo} 'pre_qFit'
+  refinement.py ${base_dir}/${holo}/${holo} ${base_dir}/${apo}/${apo} ${holo} ${apo} 'post_qFit'
   
 done <$PDB_file
