168210bb485131e56c7373dd9d83e864812acf08
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-03-22 12:58:40 -0700

    Create find_apo_holo_parallel_submit.sh

---
+++ selection_pipeline/find_apo_holo_parallel_submit.sh
@@ -0,0 +1,30 @@
+#$ -l mem_free=2G
+#$ -t 1-100
+#$ -l h_rt=100:00:00
+#$ -R yes
+#$ -V
+
+#Stephanie Wankowicz
+#https://stephaniewankowicz.github.io/
+#Fraser Lab UCSF
+
+#This script takes in a list of PDB IDs. For each PDB, it will spit out its own processes, which will run get_PDB_info_paralell.sh.
+#The folder structures for the PDBs will be /base_folder/PDB_ID/.
+
+#________________________________________________INPUTS________________________________________________
+apo_file=PDB_Apo.txt
+base_dir='/location/you/would/like/folders/of/PDBs/to/exist/'
+
+
+
+cd ${base_dir}
+mkdir potential_pairs
+
+#________________________________________________SET DEPENDENCY PATHS__________________________________
+source phenix_env.sh
+source activate qfit
+
+#________________________________________________RUN SCRIPT____________________________________________
+PDB=$(cat $apo_file | head -n $SGE_TASK_ID | tail -n 1)
+
+sh find_apo_holo_parallel.sh ${PDB} ${base_dir}
