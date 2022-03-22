dee756e6f223e1816ed21d9626ad2aefbd4181a2
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-03-22 15:29:00 -0700

    Update refinement_stat.sh

--- QC/refinement_stat.sh
+++ QC/refinement_stat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 base_dir = '/this/is/where/your/PDB/folders/are'
-PDB_file=PDB.txt #this is a list of each PDB that completed re-refinement, qFit, post qFit refinement
+pdb_pairs=holo_apo_pairs.txt # this file should have holo PDBs in column 1, apo PDBs in column 2.
 
-while read PDB; do 
+for i in {2..100}; do
   echo ${PDB}
   refinement.py ${base_dir}/${holo}/${holo} ${base_dir}/${apo}/${apo} ${holo} ${apo} 'pre_qFit'
   refinement.py ${base_dir}/${holo}/${holo} ${base_dir}/${apo}/${apo} ${holo} ${apo} 'post_qFit'
