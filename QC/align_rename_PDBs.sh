2b99e12cac1be693382044629dfc536be90e3e57
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-03-22 13:27:01 -0700

    Create align_rename_PDBs.sh

---
+++ QC/align_rename_PDBs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+'''
+This script will align two PDBs with the same sequence on top of each other. 
+Based on that alignement, it will then re-name chain IDs if needed and re-number each chain so chain A, residue 1 is referring to the same chain in each PDBs.
+All will be aligned to the apo structure. 
+'''
+
+
+#____________________________________________SOURCE REQUIREMENTS____________________________________
+source phenix_env.sh #source phenix (fill in phenix location)
+source activate qfit #conda env with qFit 
+
+#________________________________________________INPUTS________________________________________________
+base_dir='/location/you/would/like/folders/of/PDBs/to/exist/' #base folder (where you want to put folders/pdb files)
+pdb_pairs=holo_apo_pairs.txt # this file should have holo PDBs in column 1, apo PDBs in column 2.
+
+for i in {2..100}; do
+  holo=$(cat $PDB_file | awk '{ print $1 }' |head -n $i | tail -n 1)
+  apo=$(cat $PDB_file | awk '{ print $2 }' | head -n $i | tail -n 1)
+  echo 'Holo:' ${holo}
+  echo 'Apo:' ${apo}
+  cd $base_dir
+  if [ ! -f ${PDB_dir}/${holo}/${holo}_qFit.pdb ] || [ ! -f ${PDB_dir}/${apo}/${apo}_qFit.pdb ]; then
+      echo 'file not found'     
+      continue
+  else
+    pymol -c python_pymol_pre.py -- ${PDB_dir}/${holo}/${holo}_qFit.pdb ${PDB_dir}/${apo}/${apo}_qFit.pdb
+    relabel_chain.py ${PDB_dir}/${holo}/${holo}_qFit_refitted.pdb ${PDB_dir}/${apo}/${apo}_qFit.pdb ${holo} ${apo}
+    congregate_chain.py ${PDB_dir}/${apo}/${apo}_qFit_modified.pdb ${apo}
+    python renumber.py -1 ${PDB_dir}/${holo}/${holo}_qFit_refitted_renamed_modified_chain_renamed.pdb > ${PDB_dir}/${holo}/${holo}_qFit_renamed_renmbered.pdb
+    python renumber.py -1 ${PDB_dir}/${apo}/${apo}_qFit_chain_renamed.pdb > ${PDB_dir}/${apo}/${apo}_qFit_renamed_renmbered.pdb
+    
+    #now that you have aligned PDBs, determine the RMSD between their alpha carbons
+  fi
+ 
