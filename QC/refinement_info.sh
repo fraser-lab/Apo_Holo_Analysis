9a198cd1502be40d12a6d821c15e60f4d23b7bd3
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-03-22 15:34:02 -0700

    Update and rename refinement_stat.sh to refinement_info.sh

--- QC/refinement_info.sh
+++ QC/refinement_info.sh
@@ -4,8 +4,15 @@ base_dir = '/this/is/where/your/PDB/folders/are'
 pdb_pairs=holo_apo_pairs.txt # this file should have holo PDBs in column 1, apo PDBs in column 2.
 
 for i in {2..100}; do
-  echo ${PDB}
+  holo=$(cat $PDB_file | awk '{ print $1 }' |head -n $i | tail -n 1)
+  apo=$(cat $PDB_file | awk '{ print $2 }' | head -n $i | tail -n 1)
+  echo 'Holo:' ${holo}
+  echo 'Apo:' ${apo}
+  cd $base_dir
+  
   refinement.py ${base_dir}/${holo}/${holo} ${base_dir}/${apo}/${apo} ${holo} ${apo} 'pre_qFit'
   refinement.py ${base_dir}/${holo}/${holo} ${base_dir}/${apo}/${apo} ${holo} ${apo} 'post_qFit'
-  
+
 done <$PDB_file
+
+python refinement_stats.py ${base_dir}
