feb2e3556376d53cc599f9d740ec1bff0be4e512
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-03-26 19:27:52 -0700

    Update run_hbplus.sh

--- analysis/run_hbplus.sh
+++ analysis/run_hbplus.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
-#$ -l h_vmem=4G
-#$ -l mem_free=4G
-#$ -t 1-1
-#$ -l h_rt=10:00:00
-#$ -R yes
-#$ -V
 
-#source /programs/sbgrid.shrc
-#sbgrid-cli install hbplus
+#install hbplus
 source phenix
 
 PDB_file=AH_pairs.txt
 
 for i in {2..1268}; do
-  PDB=$(cat $PDB_file | awk '{ print $1 }' |head -n $i | tail -n 1)
-
-
-  cd ${PDB} #go into folder where the multiconformer structure is
-
-  #split multiconformer PDB
-  python split_multiconf.py --pdb ${PDB}_qFit.pdb --pdb_name ${PDB}
+  holo=$(cat $PDB_file | awk '{ print $1 }' |head -n $i | tail -n 1)
+  apo=$(cat $PDB_file | awk '{ print $2 }' | head -n $i | tail -n 1)
+  echo $apo $holo
   
-  #run hbplus
-  #hbplus ${PDB}_qFit_altA.pdb -h 3.2
-  #hbplus ${PDB}_qFit_altB.pdb -h 3.2
-  #hbplus ${PDB}_qFit_altC.pdb -h 3.2
-  #hbplus $PDB}_qFit_altD.pdb -h 3.2
-  #hbplus ${PDB}_qFit_altE.pdb -h 3.2
+  #run Apo
+  cd ${PDB_dir}/${apo} #go into folder where the multiconformer structure is
+  #split multiconformer PDB
+  python split_multiconf.py --pdb ${apo}_qFit.pdb --pdb_name ${apo}
+  #hbplus ${apo}_qFit_altA.pdb -h 3.2
+  #hbplus ${apo}_qFit_altB.pdb -h 3.2
+  #hbplus ${apo}_qFit_altC.pdb -h 3.2
+  #hbplus ${apo}_qFit_altD.pdb -h 3.2
+  #hbplus ${apo}_qFit_altE.pdb -h 3.2
   
   #subset to a subset of residues (see example of close residues in folder). 
-  python hbplus_subset.py ${PDB}_5.0_closeresidues.csv
+  python hbplus_subset.py ${holo}_${apo}_5.0_closeresidues.csv
+  
+  
+  #run Holo
+  cd ${PDB_dir}/${holo} #go into folder where the multiconformer structure is
+  #split multiconformer PDB
+  python split_multiconf.py --pdb ${holo}_qFit.pdb --pdb_name ${apo}
+  
+  #run hbplus
+  #hbplus ${holo}_qFit_altA.pdb -h 3.2
+  #hbplus ${holo}_qFit_altB.pdb -h 3.2
+  #hbplus ${holo}_qFit_altC.pdb -h 3.2
+  #hbplus $(holo}_qFit_altD.pdb -h 3.2
+  #hbplus ${holo}_qFit_altE.pdb -h 3.2
+  
+  #subset to a subset of residues (see example of close residues in folder). 
+  python hbplus_subset.py ${holo}_${apo}_5.0_closeresidues.csv
 done
 
